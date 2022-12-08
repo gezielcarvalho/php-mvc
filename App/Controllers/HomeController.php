04ebf4a140083fe58318deca390c27e4c53d7828
Geziel Carvalho <geziel.natal@gmail.com>
2022-12-08 09:13:38 -0300

    Create View

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -3,11 +3,12 @@
 namespace App\Controllers;
 
 use Core\Controller;
+use Core\View;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        echo "Welcome Home!";
+        View::render('Home/index.php');
     }    
 }
---
+++ App/Views/Home/index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "New view rendering approach";
