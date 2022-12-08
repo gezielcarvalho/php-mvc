276f319e5e193b0223e4d3e6a9b18bb432da9254
Geziel Carvalho <geziel.natal@gmail.com>
2022-12-08 12:33:29 -0300

    User View

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -1,11 +1,52 @@
 <?php
 
 namespace App\Controllers;
+use Core\View;
 
 class UserController {
     
     public function index()
     {
-        echo "calling users index";
+        $sidebar = [
+            'menu' => [
+                'sections' => [
+                    [
+                        'name' => 'NAVIGATION',
+                        'items' => [
+                            [
+                                'icon' => 'uil-home-alt',
+                                'title' => 'Dashboards',
+                                'href' => '/'
+                            ],
+                            [
+                                'icon' => 'uil-users-alt',
+                                'title' => 'User Management',
+                                'href' => '/users'
+                            ],
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $toolbar = [
+            'search' => [
+                'display' => 'block'
+            ]
+        ];
+        $breadcrumbs = [
+            [
+                'title' => 'Home',
+                'href' => '/'
+            ],
+            [
+                'title' => 'Navigation',
+                'href' => '/'
+            ],
+            [
+                'title' => 'User Management',
+                'href' => 'active'
+            ],
+        ];
+        View::render('Users/index.php', compact(['sidebar', 'toolbar', 'breadcrumbs']));
     }
 }
